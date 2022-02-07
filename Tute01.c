744211a4880858fc5665e0c6c2f0014c0b8658cc
Dulsha Senavirathna <99158201+Dulsharulz@users.noreply.github.com>
2022-02-07 07:43:41 +0000

    good

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,18 @@
 #include <stdio.h>
 
 int main() {
-  
+  int mark1,mark2;
+  int tot;
+  float avg;
+  printf("Enter Your 1 subject Mark:");
+  scanf("%d", &mark1);
+  printf("Enter Your 2 nd subject Mark:");
+  scanf("%d", &mark2);
+  tot=mark1+mark2;
+  avg=tot/2.0;
+  printf("Average two Number of mark:%.2f",avg);
+
+
   return 0;
 }
 
